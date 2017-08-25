fcda336f63f407bac35d9d1465a394d9232b28f8
ArtemisD <dianah456@gmail.com>
2017-08-24 22:29:59 -0400

    video list entry & video list tests pass

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,29 @@
 
 angular.module('video-player', [])
+//
+// .controller('VideoCtrl', function($scope) {
+//   $scope.videos = exampleVideoData;
+//   $scope.video = exampleVideoData[0];
+// })
 
-.controller('VideoCtrl', function($scope) {
-  $scope.videos = exampleVideoData;
-  $scope.video = exampleVideoData[0];
-})
 
 .component('app', {
-  templateUrl: 'src/templates/app.html'
+  templateUrl: 'src/templates/app.html',
+  controller: function(youTube) {
+    // this.$onInit = function() {
+      youTube.search('cats', (function(videoData) {
+        console.log('video data', videoData)
+        this.videos = videoData
+        this.currentVideo = videoData[0]
+      }).bind(this));
+    // };
+    // this.videos = exampleVideoData;
+    // this.currentVideo = exampleVideoData[0];
+    this.selectVideo = function(videoObj) {
+      this.currentVideo = videoObj;
+    };
+    this.searchResults = function() {
+
+    };
+  }
 });
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,28 @@
 angular.module('video-player')
-.service('youTube', function(){
-  // TODO
-});
+.service('youTube', function($http, $window){
+  this.search = function(query, callback) {
+    $http({
+      method: 'GET',
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      params: {
+        part: 'snippet',
+        q: query,
+        maxResults: 5,
+        key: $window.YOUTUBE_API_KEY,
+        type: 'video',
+        videoEmbeddable : true
+      }
+
+    }).then(function successCallback(response) {
+      console.log("success",response)
+      console.log('callback',callback)
+      callback(response.data.items);
+      // callback()
+    // this callback will be called asynchronously
+    // when the response is available
+    }, function errorCallback(response) {
+      console.log("error",response)
+    // called asynchronously if an error occurs    // or server returns response with an error status.
+    });
+  };
+})
